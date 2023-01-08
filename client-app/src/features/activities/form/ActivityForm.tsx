010fcdac4953a322c3ec807e2faabc369a69da23
Swapnil Somkuwar <swapnil.somkuwar44@gmail.com>
2023-01-08 21:28:44 +0530

    End of Chapter 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,11 +6,12 @@ interface Props{
     activity: Activity | undefined;
     closeForm: () => void;
     CreateOrEdit: (activity:Activity) => void;
+    submitting: boolean;
 }
 
 
 
-export default function ActivityForm({activity: selectedActivity, closeForm, CreateOrEdit}: Props){
+export default function ActivityForm({activity: selectedActivity, closeForm, CreateOrEdit, submitting}: Props){
 
     const initialState = selectedActivity ?? {
         id: '',
@@ -41,10 +42,10 @@ export default function ActivityForm({activity: selectedActivity, closeForm, Cre
                 <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
                 <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
                 <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
-                <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
+                <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
-                <Button positive floated='right' type='submit' content='Submit' />   
+                <Button loading={submitting} positive floated='right' type='submit' content='Submit' />   
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />         
 
                 </Form>
